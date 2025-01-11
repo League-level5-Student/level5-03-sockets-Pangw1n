4af7301fb62e7084d8b7b82db6910833e020caf8
league <league@imac-22.attlocal.net>
2025-01-10 18:28:51 -0800

    Working on ChatApp

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -18,14 +18,15 @@ public class ClientGreeter {
 			//4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
 			Socket socket = new Socket(ip, port);
 	        //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
-	         
+	        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 			//6. Use the DataOutputStream object to send a message to the server using the writeUTF(String message) method
-	         
+	        outputStream.writeUTF("Hello, Server");
 	        //7. Create a DataInputStream object. When initializing it, use the Server object you created in step 4 to call the getInputStream() method.
-	         
+	        DataInputStream inputStream = new DataInputStream(socket.getInputStream());
 	        //8. Use the DataInputStream object to print a message from the server using the readUTF() method.
-	         
+	        System.out.println(inputStream.readUTF());
 	        //9. Close the client's server object
+	        socket.close();
 		}
 		catch(IOException e)
 		{
--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,28 @@
 package _02_Chat_Application;
 
+import javax.swing.JOptionPane;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
 public class ChatApp {
-
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
+	public ChatApp()
+	{
+		String[] options = {"Join", "Host"};
+		int option = JOptionPane.showOptionDialog(null, "Would you like to host a room or join one", null, 0, 0, null, options, null);
+		System.out.println(option);
+		if (option == 1)
+		{
+			//host
+		}
+		else if (option == 0)
+		{
+			//join
+		}
+	}
 }
